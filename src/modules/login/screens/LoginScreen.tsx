0a3c381266568a222df8ad28a8bb3260a052255e
Thales gabriel <thalesgabriel.war@gmail.com>
2023-09-15 16:06:15 -0300

    incluso as dependencias do backend com axios

--- src/modules/login/screens/LoginScreen.tsx
+++ src/modules/login/screens/LoginScreen.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 
 import Button from "../../../shared/buttons/button/button";
@@ -12,18 +13,32 @@ import {
 } from "../styles/loginScreen.styles";
 
 const LoginScreen = () => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(event.target.value);
+  const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
   };
   const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
 
-  const handleLogin = () => {
-    console.log(username, password);
+  const handleLogin = async () => {
+    const returnObeject = await axios({
+      method: "post",
+      url: "http://localhost:8080/auth",
+      data: {
+        email: email,
+        password: password,
+      },
+    })
+      .then((result) => {
+        alert(`fez login token: ${result.data.accessToken}`);
+        return result.data;
+      })
+      .catch(() => {
+        alert("Usuario ou senha invalido");
+      });
   };
 
   return (
@@ -33,10 +48,10 @@ const LoginScreen = () => {
           <LogoImage src="./logo.png" />
           <TitleLogin level={2}>LOGIN</TitleLogin>
           <InputDefault
-            title="Username "
+            title="Email "
             margin="32px 0px 0px 0px"
-            onChange={handleUsername}
-            value={username}
+            onChange={handleEmail}
+            value={email}
           />
           <InputDefault
             title="Password"
